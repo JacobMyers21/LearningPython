13e8aa65e3785284475888384ed7c1cee9380865
Myers Jacob <813862@stma.k12.mn.us>
2021-03-25 10:18:48 -0500

    distance not working

--- sheep.py
+++ sheep.py
@@ -1,3 +1,4 @@
-def angle
-def distanceToTarget
-def firingAngle
+int input("what is the X distance?" xDistance)
+int input("what is the Y distance?" yDistance)
+int distanceToTarget = √(xDistance^2 + yDistance^2)
+print (distanceToTarget)
