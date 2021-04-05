45b2d8e7bd51698a019c84404741bc2ef2b714da
Myers Jacob <813862@stma.k12.mn.us>
2021-04-05 10:19:37 -0500

    wrinkles

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-light = input("Stoplight Color\n")
-
-if light == "Green":
-   print("GO! GO! GO!")
-elif light == "Yellow":
-   print("Theres A baby on board! HIT IT!")
-else:
-   print("Move fast with purpose my boy!")
-
-print("light")
+pies = [7, 9, 12, 13, 6000, 15, 68, 35]
+print(pies[0] * pies[1] * pies[2] * pies[3] * pies[4] * pies[5] * pies[6] * pies[7])
